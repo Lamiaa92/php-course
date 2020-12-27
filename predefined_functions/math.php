075f84a8a51f117f4bb306d17f73f09ee118a022
Lamia Moshrif <lamiamoshrif@gmail.com>
2020-12-27 10:09:49 +0100

    PHP Course

---
+++ predefined_functions/math.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //Math functions
+
+        //Ceil
+            echo ceil(0.5) . "<br>";
+
+        //Floor
+            echo floor(0.5) . "<br>";
+
+        //Round
+            echo round(0.54335, 2);
+
+            echo exp(4);
+
+
+
+    ?>
+</body>
+</html>
