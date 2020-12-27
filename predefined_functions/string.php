b4a30148ca8286b64eb3ddfa53b0d178b4750cbc
Lamia Moshrif <lamiamoshrif@gmail.com>
2020-12-27 10:30:00 +0100

    predefined string commands

--- predefined_functions/string.php
+++ predefined_functions/string.php
@@ -8,7 +8,36 @@
     <?php
     //String functions
 
+    //String length
+    $str = "Today is a sunny day";
 
+    echo strlen($str);
+
+    //String word count
+    $firstNAme = "My first name is Lamia";
+    echo "<br>";
+    echo str_word_count($firstNAme);
+    echo "<br>";
+
+    //How to find text within a string
+    $email = "lamiamoshrif@gmail.com";
+        if (strpos($email,"@")) {
+            echo "This is a valid email";
+        } else {
+            echo "This is a email input field !";
+        }
+        echo "<br>";
+
+    //Ucwords  - Every first letter of a word is uppercase
+    echo ucwords ("What is your name ?" . "<br>");
+
+    //Whole string to uppercase
+    echo strtoupper ("What is your name ?" . "<br>");
+
+    //Whole string to lowecase
+    echo strtolower ("WHAT IS YOUR NAME" . "<br>");
+
+    
     ?>
 </body>
 </html>
