d8d2a32e67b14ad27ba54889e99422caf13e4137
Lamia Moshrif <lamiamoshrif@gmail.com>
2020-12-27 16:25:45 +0100

    get

---
+++ superglobals/get.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //GET 
+    // Data will be visible in the URL.
+
+
+
+    ?>
+
+    <form action="get.php" method="get">
+        <input type="text" name="name">
+        <input type="password" name="password">
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
