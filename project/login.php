2e9222efb57f9cb797cc802f7bf4725438e84b76
Lamia Moshrif <lamiamoshrif@gmail.com>
2020-12-28 16:04:35 +0100

    loggin button named

--- project/login.php
+++ project/login.php
@@ -6,10 +6,10 @@ require_once 'include/header.php';
     <h1>Log in</h1>
     <p>No account ? <a href="register.php">Register here!</a></p>
 
-    <form action="" method="post">
+    <form action="include/login-inc.php" method="post">
         <input type="text" name="username" placeholder="Username">
         <input type="password" name="password" placeholder="Password">
-        <button type="submit">Login</button>
+        <button type="submit" name="submit">Login</button>
     </form>
 </div>
 
