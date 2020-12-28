719aa3ffe469b4371f4748d5c476f4165c53b83a
Lamia Moshrif <lamiamoshrif@gmail.com>
2020-12-28 14:23:36 +0100

    div added

--- project/login.php
+++ project/login.php
@@ -2,15 +2,16 @@
 require_once 'include/header.php';
 ?>
 
-<h1>Log in</h1>
-<p>No account ? <a href="register.php">Register here!</a></p>
+<div>
+    <h1>Log in</h1>
+    <p>No account ? <a href="register.php">Register here!</a></p>
 
-<form action="" method="post">
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="password" placeholder="Password">
-    <button type="submit">Login</button>
-</form>
-
+    <form action="" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <button type="submit">Login</button>
+    </form>
+</div>
 <?php
 require_once 'include/footer.php';
 ?>
