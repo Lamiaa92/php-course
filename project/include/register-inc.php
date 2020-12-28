7188101a0a37e2a0a8cd2aedb9b32eb044ec0238
Lamia Moshrif <lamiamoshrif@gmail.com>
2020-12-28 14:53:25 +0100

    Error handler added

--- project/include/register-inc.php
+++ project/include/register-inc.php
@@ -7,6 +7,10 @@ if (isset($_POST['submit'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $confirmPass = $_POST['confirmPassword'];
+
+    if (empty($username) || empty($password) || empty($confirmPass)) {
+        header("Location: ../register.php?error=emptyfields&username=".$username);
+    }
 }
 
 
