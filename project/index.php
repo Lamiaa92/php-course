ec2eafa5d035e20e66eaa06b0d5dc5a16cff02c2
Lamia Moshrif <lamiamoshrif@gmail.com>
2020-12-28 16:03:04 +0100

    home page changed

--- project/index.php
+++ project/index.php
@@ -2,7 +2,13 @@
 require_once 'include/header.php';
 ?>
 
-HOME
+<?php
+    if (isset($_SESSION['sessionId'])) {
+        echo "You are logged in!";
+    } else {
+        echo "Home";
+    }
+?>
 
 <?php
 require_once 'include/footer.php';
