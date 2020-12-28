0336ac41aab57bb3a49fa716b54fc9ec84e20abf
Lamia Moshrif <lamiamoshrif@gmail.com>
2020-12-28 15:25:00 +0100

    prepared statements

--- project/include/register-inc.php
+++ project/include/register-inc.php
@@ -18,8 +18,35 @@ if (isset($_POST['submit'])) {
         header("Location: ../register.php?error=passwordsdonotmatch&username=".$username);
         exit();
     } else {
-        $sql = "";
+        $sql = "SELECT username FROM users WHERE username = ? ";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: ../register.php?error=sqlerror");
+            exit();
+        } else {
+            mysqli_stmt_bind_param($stmt, "s", $username);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_store_result($stmt);
+            $rowCount = mysqli_stmt_num_rows($stmt);
+
+            if($rowCount > 0) { 
+                header("Location: ../register.php?error=usernametaken");
+                exit();
+            } else {
+                $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+                $stmt = mysqli_stmt_init($conn);
+                if (!mysqli_stmt_prepare($stmt, $sql)) {
+                    header("Location: ../register.php?error=sqlerror");
+                    exit();
+                } else {
+                    mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+                    mysqli_stmt_execute($stmt);
+                    mysqli_stmt_store_result($stmt);
+                }
+            }
+        }
     }
+
 }
 
 
