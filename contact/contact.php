1f51dac417c6255671e9cce31548d786a90605e0
Lamia Moshrif <lamiamoshrif@gmail.com>
2020-12-28 10:23:01 +0100

    backend of the form

---
+++ contact/contact.php
@@ -0,0 +1,23 @@
+<?php
+
+//Trim removes all the white space before/after your name.
+
+if (isset($_POST['submit'])) {
+    $name = trim($_POST['name']);
+    $email = trim($_POST['email']);
+    $subject = trim($_POST['subject']);
+    $message = trim($_POST['message']);
+
+    $myMail = "lamiamoshrif@gmail.com";
+    $header = "From: " . $email;
+    $message2 = "You have received a message from " . $name . ". \n\n" . $message;
+
+    //1. E-mail your sending it to
+    //2. Subject
+    //3. Message
+    mail($myMail, $subject, $message2, $header);
+    header("Location: index.php?mailsend");
+}
+
+?>
+
